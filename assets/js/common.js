a37018eceb0f517e24b2df6c55f62efccc2d1261
praveen-webartisan <67270359+praveen-webartisan@users.noreply.github.com>
2023-02-14 01:13:14 +0530

    Hobbies section updated, Links updated

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,6 +7,18 @@
 		$(this).toggleClass('is-active');
 	});
 
+	$(document).on('click', '.modal-background, .modal-close', function(e) {
+		e.preventDefault();
+
+		$(this).closest('.modal').removeClass('is-active');
+	});
+
+	$(document).on('click', '.open-modal[data-target]', function(e) {
+		e.preventDefault();
+
+		$('.modal' + $(this).attr('data-target')).addClass('is-active');
+	});
+
 	function isValidContactForm() {
 		let formData, subject;
 		let contactForm = $('#contactForm');
